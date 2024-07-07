9e9de4b225e631baaa0f6a59abee4b30eb9c2d8e
Usman Abdullah <usmanabdullahbn@gmail.com>
2024-07-07 16:09:48 +0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -35,6 +35,13 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next(); // Pass control to the next handler
+});
+
 app.get("/", (req, res) => {
   res.send("API WOking with /api/v1");
 });
