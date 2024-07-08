f42863d7195732036f823e64d5911338431d2d48
Usman Abdullah <usmanabdullbn@gmail.com>
2024-07-08 13:16:20 +0500

    Includes Dist folder

---
+++ dist/routes/order.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { adminOnly } from "../middlewares/auth.js";
+import { allOrders, deleteOrder, getSingnalOrder, myOrders, newOrder, processOrder, } from "../controllers/order.js";
+const app = express.Router();
+// Route - /api/v1/order/new
+app.post("/new", newOrder);
+// Route - /api/v1/order/my
+app.get("/my", myOrders);
+// Route - /api/v1/order/all
+app.get("/all", adminOnly, allOrders);
+app.route("/:id").get(getSingnalOrder).put(adminOnly, processOrder).delete(adminOnly, deleteOrder);
+export default app;
---
+++ dist/routes/products.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { adminOnly } from "../middlewares/auth.js";
+import { singleUpload } from "../middlewares/multer.js";
+import { deleteProduct, getAdmintProducts, getAllCategories, getAllProducts, getLastestProducts, getSigleProduct, newProduct, updateProduct, } from "../controllers/product.js";
+const app = express.Router();
+// To Create New Product - /api/v1/product/new
+app.post("/new", adminOnly, singleUpload, newProduct);
+// To get all Products with filters - /api/v1/product/all
+app.get("/all", getAllProducts);
+// To get last 10 Products - /api/v1/product/latest
+app.get("/latest", getLastestProducts);
+// To Create all unique Categories - /api/v1/product/categories
+app.get("/categories", getAllCategories);
+// To Create get all Product - /api/v1/product/admin-products
+app.get("/admin-products", adminOnly, getAdmintProducts);
+// To get Update, Delete Product!
+app
+    .route("/:id")
+    .get(getSigleProduct)
+    .put(adminOnly, singleUpload, updateProduct)
+    .delete(adminOnly, deleteProduct);
+export default app;
