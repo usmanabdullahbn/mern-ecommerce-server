e6e1eb9bb836c53a964bf71ce1310088a99eecda
Usman Abdullah <usmanabdullbn@gmail.com>
2024-08-19 13:56:10 +0500

    getAllProduct

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -206,7 +206,8 @@ export const getAllProducts = TryCatch(
       Product.find(baseQuerry),
     ]);
 
-    const totalPage = Math.ceil(products.length / limit);
+    const totalProducts = filteredOnlyProducts.length;
+    const totalPage = Math.ceil(totalProducts / limit);
 
     return res.status(200).json({
       success: true,
@@ -216,36 +217,3 @@ export const getAllProducts = TryCatch(
   }
 );
 
-// const generateRandomProducts = async (count: number = 10) => {
-//   const products = [];
-
-//   for (let i = 0; i < count; i++) {
-//     const product = {
-//       name: faker.commerce.productName(),
-//       photo: "uploads\\9e54d0d9-d934-46d5-a00e-4da0941aeb42.webp",
-//       price: faker.commerce.price({ min: 1500, max: 80000, dec: 0 }),
-//       stock: faker.commerce.price({ min: 0, max: 100, dec: 0 }),
-//       category: faker.commerce.department(),
-//       createdAt: new Date(faker.date.past()),
-//       updatedAt: new Date(faker.date.recent()),
-//       __v: 0,
-//     };
-
-//     products.push(product);
-//   }
-
-//   await Product.create(products);
-
-//   console.log({ succecss: true });
-// };
-
-// const deleteRandomsProducts = async (count: number = 10) => {
-//   const products = await Product.find({}).skip(2);
-
-//   for (let i = 0; i < products.length; i++) {
-//     const product = products[i];
-//     await product.deleteOne();
-//   }
-
-//   console.log({ succecss: true });
-// };
